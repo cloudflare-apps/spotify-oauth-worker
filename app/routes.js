98f5a5d2cdfe4616ec968c8e0accdda3d0e8ddf9
Victoria <bernard.victoria22@gmail.com>
2019-02-11 12:37:41 -0600

    Turn node server into a worker

--- app/routes.js
+++ app/routes.js
@@ -1,160 +1,207 @@
-const { getJson } = require("simple-fetch");
-const { assign } = Object;
+const getJson = async function(url) {
+  let response = await fetch(url);
+  return response.json();
+};
 const DEFAULT_PLAYLIST_SCHEMA = {
   enum: ["custom"],
   enumNames: {
-    custom: "Choose a playlist from a URI..."
+    custom:  "Choose a playlist from a URI..."
   }
 };
 
 const DEFAULT_ARTIST_SCHEMA = {
   enum: ["custom"],
   enumNames: {
-    custom: "Choose an artist from a URI..."
+    custom: "Choose an artist (from woerker) from a URI..."
   }
 };
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
 
-module.exports = function setRoutes(app) {
-  // Primary OAuth request handler.
-  // This handler fetches the user's Spotify playlists and followed artists,
-  // then populates an install field with the entries.
-  app.post("/", function(request, response) {
-    const { install } = request.body;
-    if (!request.body.metadata.newValue) {
-      // User has logged out. Reset schema.
+/**
+ * Fetch and log a given request object
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  app = {
+    get: (endpoint, fn) => {
+      url = new URL(request.url);
+      if (url.pathname === endpoint && request.method === "GET")
+        return fn(request);
+      return null;
+    },
+    post: (endpoint, fn) => {
+      url = new URL(request.url);
+      console.log(url);
+      if (url.pathname === endpoint && request.method === "POST")
+        return fn(request);
+      return null;
+    }
+  };
+  // ret is the return path the request hits
+  let ret = null;
 
-      assign(
-        install.schema.properties.widgets.items.properties.playlist.properties
-          .URI,
-        DEFAULT_PLAYLIST_SCHEMA
-      );
+  while (!ret) {
+    // Primary OAuth request handler.
+    // This handler fetches the user's Spotify playlists and followed artists,
+    // then populates an install field with the entries.
+    ret = app.post("/", async function(request) {
+      body = await request.json();
+      const { install } = body;
+      // return new Response(JSON.stringify({ install, proceed: true }))
 
-      install.options.widgets.forEach(widget => {
-        widget.playlist.URI = "custom";
-      });
+      if (!body.metadata.newValue) {
+        // User has logged out. Reset schema.
 
-      assign(
-        install.schema.properties.widgets.items.properties.artist.properties
-          .URI,
-        DEFAULT_ARTIST_SCHEMA
-      );
+        Object.assign(
+          install.schema.properties.widgets.items.properties.playlist.properties
+            .URI,
+          DEFAULT_PLAYLIST_SCHEMA
+        );
 
-      install.options.widgets.forEach(widget => {
-        widget.artist.URI = "custom";
-      });
+        install.options.widgets.forEach(widget => {
+          widget.playlist.URI = "custom";
+        });
 
-      response.json({ install, proceed: true });
-      return;
+        Object.assign(
+          install.schema.properties.widgets.items.properties.artist.properties
+            .URI,
+          DEFAULT_ARTIST_SCHEMA
+        );
+
+        install.options.widgets.forEach(widget => {
+          widget.artist.URI = "custom";
+        });
+        return new Response(JSON.stringify({ install, proceed: true }));
+      }
+      const auth = body.authentications.account.token;
+
+      // Include link to Spotify player.
+      install.links = [
+        {
+          title: "Spotify",
+          description:
+            "Visit Spotify to manage your playlists and followed artists.",
+          href: "https://www.spotify.com"
+        }
+      ];
+      const playlistsPromise = await fetch(
+        "https://api.spotify.com/v1/me/playlists?limit=50",
+        {
+          headers: {
+            authorization: `${auth.type} ${auth.token}`
+          }
+        }
+      )
+        .then(res => {
+          return res.json();
+        })
+        .then(res => {
+          const { items = [] } = res;
+          const playlistSchema = Object.assign({}, DEFAULT_PLAYLIST_SCHEMA);
+
+          items.sort((a, b) => a.name.localeCompare(b.name));
+
+          items.forEach(item => {
+            playlistSchema.enum.push(item.uri);
+            playlistSchema.enumNames[item.uri] = item.name;
+          });
+          Object.assign(
+            install.schema.properties.widgets.items.properties.playlist
+              .properties.URI,
+            playlistSchema
+          );
+          install.options.widgets.forEach(widget => {
+            widget.playlist.URI = playlistSchema.enum[1];
+          });
+        });
+
+      const artistsPromise = fetch(
+        "https://api.spotify.com/v1/me/following?type=artist&limit=50",
+        {
+          headers: {
+            authorization: `${auth.type} ${auth.token}`
+          }
+        }
+      )
+        .then(res => res.json())
+        .then(res => {
+          const { items = [] } = res.artists;
+          const artistSchema = Object.assign({}, DEFAULT_ARTIST_SCHEMA);
+
+          items.sort((a, b) => a.name.localeCompare(b.name));
+
+          items.forEach(item => {
+            artistSchema.enum.push(item.uri);
+            artistSchema.enumNames[item.uri] = item.name;
+          });
+          Object.assign(
+            install.schema.properties.widgets.items.properties.artist.properties
+              .URI,
+            artistSchema
+          );
+
+          install.options.widgets.forEach(widget => {
+            widget.artist.URI = artistSchema.enum[1];
+          });
+        });
+
+      ret = await Promise.all([playlistsPromise, artistsPromise])
+        .catch(error => {
+          return new Response(
+            JSON.stringify({
+              proceed: false,
+              errors: [{ type: "400", message: error.toString() }]
+            })
+          );
+        })
+        .then(() => {
+          return new Response(JSON.stringify({ install, proceed: true }));
+        });
+      return ret;
+    });
+    if (ret) {
+      return ret;
     }
 
-    const auth = request.body.authentications.account.token;
-
-    // Include link to Spotify player.
-    install.links = [
-      {
-        title: "Spotify",
-        description:
-          "Visit Spotify to manage your playlists and followed artists.",
-        href: "https://www.spotify.com"
-      }
-    ];
-
-    const playlistsPromise = getJson(
-      "https://api.spotify.com/v1/me/playlists?limit=50",
-      {
+    // Account metadata handler.
+    // This handler fetches user info and populates the login entry with user's info.
+    ret = app.get("/account-metadata", function(request) {
+      return fetch("https://api.spotify.com/v1/me", {
         headers: {
-          authorization: `${auth.type} ${auth.token}`
+          authorization: request.headers.authorization
         }
-      }
-    ).then(res => {
-      const { items = [] } = res;
-      const playlistSchema = assign({}, DEFAULT_PLAYLIST_SCHEMA);
-
-      items.sort((a, b) => a.name.localeCompare(b.name));
-
-      items.forEach(item => {
-        playlistSchema.enum.push(item.uri);
-        playlistSchema.enumNames[item.uri] = item.name;
-      });
-
-      assign(
-        install.schema.properties.widgets.items.properties.playlist.properties
-          .URI,
-        playlistSchema
-      );
-
-      install.options.widgets.forEach(widget => {
-        widget.playlist.URI = playlistSchema.enum[1];
-      });
-    });
-
-    const artistsPromise = getJson(
-      "https://api.spotify.com/v1/me/following?type=artist&limit=50",
-      {
-        headers: {
-          authorization: `${auth.type} ${auth.token}`
-        }
-      }
-    ).then(res => {
-      const { items = [] } = res.artists;
-      const artistSchema = assign({}, DEFAULT_ARTIST_SCHEMA);
-
-      items.sort((a, b) => a.name.localeCompare(b.name));
-
-      items.forEach(item => {
-        artistSchema.enum.push(item.uri);
-        artistSchema.enumNames[item.uri] = item.name;
-      });
-
-      assign(
-        install.schema.properties.widgets.items.properties.artist.properties
-          .URI,
-        artistSchema
-      );
-
-      install.options.widgets.forEach(widget => {
-        widget.artist.URI = artistSchema.enum[1];
-      });
-    });
-
-    Promise.all([playlistsPromise, artistsPromise])
-      .catch(error => {
-        response.json({
-          proceed: false,
-          errors: [{ type: "400", message: error.toString() }]
-        });
       })
-      .then(() => {
-        response.json({ install, proceed: true });
-      });
-  });
-
-  // Account metadata handler.
-  // This handler fetches user info and populates the login entry with user's info.
-  app.get("/account-metadata", function(request, response) {
-    getJson("https://api.spotify.com/v1/me", {
-      headers: {
-        authorization: request.headers.authorization
-      }
-    })
-      .catch(error => {
-        response.json({
-          proceed: false,
-          errors: [{ type: "400", message: error.toString() }]
+        .catch(error => {
+          return new Response(
+            JSON.stringify({
+              proceed: false,
+              errors: [{ type: "400", message: error.toString() }]
+            })
+          );
+        })
+        .then(res => {
+          return new Response(
+            JSON.stringify({
+              metadata: {
+                email: res.email,
+                username: res.display_name || res.id,
+                userId: res.id
+              }
+            })
+          );
         });
-      })
-      .then(res => {
-        response.json({
-          metadata: {
-            email: res.email,
-            username: res.display_name || res.id,
-            userId: res.id
-          }
-        });
-      });
-  });
-
-  app.get("/healthcheck", function(request, response) {
-    response.sendStatus(200);
-  });
-};
+    });
+    if (ret) {
+      return ret;
+    }
+    ret = app.get("/healthcheck", function(request, response) {
+      return new Response(200);
+    });
+    if (ret) {
+      return ret;
+    }
+    return fetch(request);
+  }
+}
